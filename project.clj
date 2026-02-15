3b560f8d41ea7f6b27a1de6bf245904205931fc5
Charles Hughes <cjhughes87@pm.me>
2026-02-14 18:18:41 -0800

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject fractal-explorer "0.1.0-SNAPSHOT"
+  :description "Interactive fractal art generator in Clojure"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [quil "4.3.1563"]]
+  :main fractal-explorer.core)
